cdec0b74194d9915b5bc4d03ec414f1c32ad4330
fire38 <skell83@naver.com>
2023-07-30 16:44:08 +0900

    123

--- src/pages/include/Header.js
+++ src/pages/include/Header.js
@@ -5,7 +5,7 @@ const Header = (props) => {
         {
           id: 1,
           title: '소개',
-          link : '/intro/about',
+          link:'',
           className : 'link menu1',
           subMenu: [
             { id: 11, title: '서비스 소개', link:'/intro/About' },
@@ -16,7 +16,7 @@ const Header = (props) => {
         {
           id: 2,
           title: '지진관측소',
-          link : '/observatory',
+          link:'',
           className : 'link menu2',
           subMenu: [
             { id: 21, title: '관측소 정보', link:'/observatory/stations' },
@@ -26,7 +26,7 @@ const Header = (props) => {
         {
           id: 3,
           title: '지진관측자료',
-          link:'/data',
+          link:'',
           className : 'link menu3',
           subMenu: [
             { id: 31, title: '이벤트파형자료', link:'/data/event-data' },
@@ -44,59 +44,39 @@ const Header = (props) => {
             { id: 42, title: '지진학술연보' , link:'/analysis/annual-report'},
             { id: 43, title: 'K-ESM DB', link:'/analysis/kesmdb' },
           ],
-        },
-        {
-          id: 5,
-          title: '로그인',
-          className : 'btn_login',
-          link:'',
-          subMenu: [],
-        },
-        {
-          id: 6,
-          title: 'English',
-          className : 'btn_language',
-          link:'',
-          subMenu: [],
-        },
+        }
       ];
-    const [openSubMenuId, setOpenSubMenuId] = useState(null);
-    const [activeMenuId, setActiveMenuId] = useState(null);
-    const [isHamToggle, setHamToggle] = useState(false);
+    const [openSubMenuId, setOpenSubMenuId] = useState(null); 
     const [isGnbToggle, setGnbToggle] = useState(false); 
     const hambugerMenuSwitchHandler = () => {
-        setHamToggle(!isHamToggle);
+      setGnbToggle(!isGnbToggle);
+      setOpenSubMenuId(1);
     };
     const gnbMenuSwitchHandler = (val) => {
         setGnbToggle(val);
     };
     const handleMenuClick = (id) => {
-      setOpenSubMenuId((prevId) => (prevId === id ? null : id));
-    };
-  
-    const handleMouseEnter = (id) => {
-      setActiveMenuId(id);
+      setOpenSubMenuId(id); 
       gnbMenuSwitchHandler(true);
     };
-  
-    const handleMouseLeave = () => {
-      setActiveMenuId(null);
+   
+    const handleMouseLeave = () => { 
       gnbMenuSwitchHandler(false);
+      setOpenSubMenuId(null)
     };
     return (
         <>
             <p className="a11y_link"><a href="#body_container">본문 바로가기</a></p>
-            <header className={'header_container  active' + props.subPageCheck}>{/* 오픈 상태일때는 active */}
+            <header onMouseLeave={handleMouseLeave}  className={isGnbToggle ? 'header_container active ' + props.subPageCheck : 'header_container ' + props.subPageCheck  }>{/* 오픈 상태일때는 active */}
                 <div className="inner">
                     <h1 className="logo"><Link to="/" className="parents">지오빅데이터 오픈플랫폼</Link><Link to="/" className="index">지진연구정보</Link></h1>
-                    <nav className={isHamToggle && isGnbToggle ? "close" : ""}> {/* 오픈 상태일때는 close */}
+                    <nav className={ isGnbToggle ? "close" : ""}> {/* 오픈 상태일때는 close */}
                         <ul>
                         {menuData.map((menu) => (
                             <li
                                 key={menu.id}
-                                className={activeMenuId === menu.id || openSubMenuId === menu.id ? 'on' : ''}
-                                onMouseEnter={() => handleMouseEnter(menu.id)}
-                                onMouseLeave={handleMouseLeave}
+                                className={ openSubMenuId === menu.id ? 'on' : ''}
+                                onMouseOver={() => handleMenuClick(menu.id)}
                                 onClick={() => handleMenuClick(menu.id)}
                             >
                                 <Link className={menu.className} to={menu.link}> {menu.title}</Link> 
@@ -110,10 +90,20 @@ const Header = (props) => {
                                 </div>
                                 )}
                             </li>
-                            ))} 
+                            ))}
+                            <li className="login_menu">
+                                <button type="button" className="btn_login">로그인</button>
+                                <div className="sub_menu">
+                                    <p><strong>홍길동</strong>님</p>
+                                    <Link to="#" className="btn_member">회원정보변경</Link>
+                                </div>
+                            </li>
+                            <li>
+                                <button type="button" className="btn_language">English</button>
+                            </li>
                         </ul>
                     </nav>
-                    <button type="button" className="btn_ham" onClick={hambugerMenuSwitchHandler}><span>{isHamToggle  ? '메뉴 닫기' : '메뉴 열기'}</span></button>
+                    <button type="button" className="btn_ham" onClick={hambugerMenuSwitchHandler}><span>{isGnbToggle  ? '메뉴 닫기' : '메뉴 열기'}</span></button>
                 </div>
             </header>
         </>
